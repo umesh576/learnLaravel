e413bf320632f161c9cfb52469b80dc03108e1cc
Umesh Joshi <uj19343@gmail.com>
2025-07-29 13:03:31 +0545

    learn about the named route in dymanic page

--- resources/views/url/Profile.blade.php
+++ resources/views/url/Profile.blade.php
@@ -1,4 +1,4 @@
 <div>
     <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-     <h1>this is profile of: </h1>
+     <h1>this is profile of: {{  }}</h1>
 </div>
